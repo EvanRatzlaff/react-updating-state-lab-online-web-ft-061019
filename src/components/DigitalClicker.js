189c89e4162213402cda9fe9de207b43d5ce4a8d
Evan_Ratzlaff <ratzlaff92@gmail.com>
2019-10-22 21:00:39 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,27 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+export default class DigitalClicker extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            timesClicked: 0
+        }
+    }
+
+    onClick = () => {
+        const increment = this.state.timesClicked + 1
+        this.setState({
+            timesClicked: increment
+        })
+    }
+
+    render() {
+        return(
+            <button onClick={this.onClick}>
+                <label>{this.state.timesClicked}</label>
+            </button>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,52 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+    constructor(){
+        super()
+        this.state = {
+            errors: [],
+            user: null,
+            settings: {
+                bitrate: 8,
+                video: {
+                    resolution: '1080p'
+                }
+            }
+        }
+    }
+
+    onClick = () => {
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                bitrate: 12
+            }
+        })
+    }
+    resButton = () => {
+        let newRes = '720p'
+        this.setState({
+            settings: {
+                ...this.state.settings,
+                video: {
+                    resolution: newRes
+                }
+            }
+        })
+    }
+
+    render() {
+        return(
+            <div>
+            <button className="bitrate" onClick={this.onClick}>
+                <label>Press Here</label>
+            </button>
+            <button className="resolution" onClick={this.resButton}>
+                <label>Press Here!</label>
+            </button>
+            </div>
+        )
+    }
+
+}
